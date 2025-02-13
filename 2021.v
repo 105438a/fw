0467deddacc0e87a6ac15b76b50a17f0d61b6014
105438a <103987662+105438a@users.noreply.github.com>
2025-02-13 18:20:33 +0800

    Update and rename 2021_IDLE_problem.v to 2021.v

--- 2021.v
+++ 2021.v
@@ -7,7 +7,7 @@ output reg valid;
 output reg is_inside;
 
 parameter IDLE = 2'd0, SORT = 2'd1, CAL = 2'd2, FINISH = 2'd3;
-reg [2:0] cst, nst;
+reg [1:0] cst, nst;
 
 //read
 reg [2:0] counter;
@@ -34,9 +34,11 @@ always @(posedge clk or posedge reset) begin
     else if (cst == IDLE) begin
         valid <= 1'b0;
         is_inside <= 1'b1;
-        x[counter] <= X;
-        y[counter] <= Y;
-        counter <= counter + 3'd1;
+        if (!valid) begin
+            x[counter] <= X;
+            y[counter] <= Y;
+            counter <= counter + 3'd1;
+        end
     end
     else begin
         counter <= 3'd0;
@@ -44,40 +46,60 @@ always @(posedge clk or posedge reset) begin
 end
 
 //sort
-reg [2:0] point_sort;
+reg [2:0] i, j;
 wire [2:0] cnt1;
-assign cnt1 = point_sort - 3'd1;
+assign cnt1 = j - 3'd1;
+wire signed [10:0] Ax, Bx, Ay, By;
+assign Ax = x[cnt1]-x[1];
+assign Ay = y[cnt1]-y[1];
+assign Bx = x[j]-x[1];
+assign By = y[j]-y[1];
+wire [21:0] out;
+assign out = (Ax*By - Bx*Ay);
 always @(posedge clk or posedge reset) begin
     if (reset) begin
-        point_sort <= 3'd3;
+        i <= 3'd3;
+        j <= 3'd3;  
     end
     else if (cst == SORT) begin
-        if (((x[cnt1] - x[1])*(y[point_sort] - y[1])) - ((x[point_sort] - x[1])*(y[cnt1] - y[1])) > 0) begin
-            x[point_sort] <= x[cnt1];
-            y[point_sort] <= y[cnt1];
-            x[cnt1] <= x[point_sort];
-            y[cnt1] <= y[point_sort];
+        if (cnt1 == 3'd2) begin
+            i <= i + 3'd1;
+            j <= i + 3'd1;
         end
-        point_sort <= point_sort + 3'd1;
+        else j <= j - 3'd1;
     end
-    else point_sort <= 3'd3;
+    else begin
+        i <= 3'd3;
+        j <= 3'd3;
+    end
+end
+
+always @(posedge clk) begin
+    if (cst == SORT) begin
+        if (out[21] == 1'b0) begin
+            x[j] <= x[cnt1];
+            y[j] <= y[cnt1];
+            x[cnt1] <= x[j];
+            y[cnt1] <= y[j];
+        end
+    end
 end
 
 //cal
 reg [2:0] point_cal;
 wire [2:0] cnt2;
 assign cnt2 = point_cal - 3'd1;
+wire [21:0] check;
+assign check = (((x[cnt2] - x[0])*(y[point_cal] - y[cnt2])) - ((x[point_cal] - x[cnt2])*(y[cnt2] - y[0])));
 always @(posedge clk or posedge reset) begin
     if (reset) begin
         point_cal <= 3'd2;
         is_inside <= 1'b1;
     end
     else if (cst == CAL) begin
-        if ((((x[cnt2] - x[0])*(y[point_cal] - y[0])) - ((x[point_cal] - x[0])*(y[cnt2] - y[0]))) < 0) begin
+        if (check[21] == 1'b0) begin
             is_inside <= 1'b0;
         end
-        else is_inside <= 1'b1;
-
         point_cal <= point_cal + 3'd1;
     end
     else begin
@@ -111,7 +133,7 @@ always @(*) begin
             else nst = IDLE;
         end 
         SORT: begin
-            if (point_sort == 3'd6) begin
+            if (i == 3'd6 && j == 3'd3) begin
                 nst = CAL;
             end
             else nst = SORT;
@@ -129,4 +151,3 @@ always @(*) begin
     endcase
 end
 endmodule
-
